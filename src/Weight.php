fc30596cb93793d87d2e1fb1512a7a8efe0ba8e0
obadadk <obadadk@users.noreply.github.com>
2022-02-11 12:38:53 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -3,15 +3,16 @@
 namespace Obadadk\PhpPackageTraining;
 
 class Weight
-{   
+{
     private float $kilograms;
 
-    static function fromKilograms(float $kilograms):self
+    public static function fromKilograms(float $kilograms): self
     {
         return new static($kilograms);
     }
 
-    public function __construct(float $kilograms) {
+    public function __construct(float $kilograms)
+    {
         $this->kilograms = $kilograms;
     }
 
